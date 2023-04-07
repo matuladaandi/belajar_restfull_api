f0687c936eec6a9093558e933663b283a0b861ad
andi <echo.matuladaandi@gmail.com>
2023-04-07 14:23:43 +0800

    menerapkan maxline,overflow dan softwrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() 
-{
-  
-  runApp( new MyApp());
-  
+void main() {
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       home: Scaffold(
-        appBar: AppBar(title: Text('My App Learn'),),
-        body: Text('hello word'),
+        appBar: AppBar(
+          title: Text('My App Learn'),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.red.shade200,
+                width: 150,
+                height: 50,
+                child: Text(
+                  'saya sedang melatih kemampuan flutter saya secara otodidak',
+                  maxLines: 2,
+                  overflow: TextOverflow.clip ,
+                  softWrap: false,
+                ))),
       ),
     );
   }
-
-}                   
+}
