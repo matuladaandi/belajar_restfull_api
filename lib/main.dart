6ff4f3b33ca62de82647cadd43a9585fe013c170
andi <echo.matuladaandi@gmail.com>
2023-04-15 14:07:18 +0800

    border radius atau style pada border

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
           margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
           child: Container(
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
                 gradient: LinearGradient(
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
