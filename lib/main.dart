aa69b1c4a7ed044d69e503b8ff881db92a130dd1
andi <echo.matuladaandi@gmail.com>
2023-04-15 23:43:15 +0800

    statefulWidget start

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,17 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Container 2"),
-        ),
-        body: Container(
-          color: Colors.red[200],
-          margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: <Color>[Colors.amber, Colors.blueAccent])),
-            margin: EdgeInsets.all(10),
-          ),
-        ),
-      ),
+    return Container(
+
     );
   }
 }
