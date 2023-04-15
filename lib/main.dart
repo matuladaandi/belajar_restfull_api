1b11227a9f755d4a3bcb3a1b33181c4a3ad24106
andi <echo.matuladaandi@gmail.com>
2023-04-15 13:59:34 +0800

    2 container dan decoration

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Container"),
+          title: Text("Latihan Container 2"),
         ),
         body: Container(
           color: Colors.red[200],
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(20),
-          child: Container(color: Colors.blue),
+          margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: <Color>[Colors.amber, Colors.blueAccent])),
+            margin: EdgeInsets.all(10),
+          ),
         ),
       ),
     );
