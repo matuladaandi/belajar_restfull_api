47d7770ef4537da41366467d101053e6996cdf11
andi <echo.matuladaandi@gmail.com>
2023-04-16 15:15:41 +0800

    membuat widget statefull yaitu membuat sebuah program sederhana jika di tekan tombol maka akan bertambah akan dan berkurang serta sedikit style

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,47 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int number = 0;
+  int kurang = 0;
+  
+  void kurangBil(){
+    setState(() {
+      kurang = number-1 ;
+    });
+  }
+  void tekanTombol() {
+    setState(() {  // agar ke reload / diubah
+      number = number+1 ;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("State Full"),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(number.toString(),style: TextStyle(color: Colors.green, fontSize: 10 + number.toDouble()),),
+            Text(kurang.toString(), style: TextStyle(color: Colors.red),),
+            ElevatedButton(
+              child: Text("Tambah Bilangan"),
+              onPressed: tekanTombol,
+            ),
+            ElevatedButton(onPressed: kurangBil, child: Text('Kurangi 1'))
+          ],
+         ),
+        ),
+      ),
     );
   }
 }
