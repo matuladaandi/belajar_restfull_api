28a431bf207a33ac265e90027a1cf947fc2f59fc
andi <echo.matuladaandi@gmail.com>
2023-04-16 17:52:01 +0800

    stateWidget start style

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,12 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
 
-class _MyAppState extends State<MyApp> {
-  String message = 'ini adalah Teks';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Anonymous Method"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(message),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      message = 'Tombol sudah diTekan';
-                    });
-                  },
-                  child: Text("Tekan !"))
-            ],
-          ),
-        ),
-      ),
+      
     );
   }
 }
