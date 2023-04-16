5b8df601d166f18dafbf3e4795801be4f002e6b2
andi <echo.matuladaandi@gmail.com>
2023-04-16 17:32:57 +0800

    Anonymos method atau method yg tdk ada nama

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int number = 0;
-  int kurang = 0;
-  
-  void kurangBil(){
-    setState(() {
-      kurang = number-1 ;
-    });
-  }
-  void tekanTombol() {
-    setState(() {  // agar ke reload / diubah
-      number = number+1 ;
-    });
-  }
-
+  String message = 'ini adalah Teks';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("State Full"),
+          title: Text("Anonymous Method"),
         ),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(number.toString(),style: TextStyle(color: Colors.green, fontSize: 10 + number.toDouble()),),
-            Text(kurang.toString(), style: TextStyle(color: Colors.red),),
-            ElevatedButton(
-              child: Text("Tambah Bilangan"),
-              onPressed: tekanTombol,
-            ),
-            ElevatedButton(onPressed: kurangBil, child: Text('Kurangi 1'))
-          ],
-         ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      message = 'Tombol sudah diTekan';
+                    });
+                  },
+                  child: Text("Tekan !"))
+            ],
+          ),
         ),
       ),
     );
