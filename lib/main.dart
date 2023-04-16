5a6870b7234e05cb903975de4bd6d643c1865286
andi <echo.matuladaandi@gmail.com>
2023-04-16 22:31:54 +0800

    tambah style dan fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stylee"),
+        ),
+        body: Center(
+          child: Text(
+            'ini adalah text',
+            style: TextStyle(fontFamily: "Raleway-Italic-VariableFont_wght",fontSize: 20),
+          ),
+        ),
+      ),
     );
   }
 }
