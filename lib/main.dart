27dd39889b5b0aeacb20b74f6cf0f15124eb55b3
andi <echo.matuladaandi@gmail.com>
2023-04-17 14:32:36 +0800

    membuat listView

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,28 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgetsku = []; // membuat list
+
+_MyAppState(){
+  for(int i =0; i <= 40; i++){
+  widgetsku.add(Text("data ke- " + i.toString()));
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Stylee"),
+          title: Text("Latihan ListView"),
         ),
-        body: Center(
-          child: Text(
-            'ini adalah text',
-            style: TextStyle(fontFamily: "Raleway-Italic-VariableFont_wght",fontSize: 20),
-          ),
-        ),
+        body: ListView(children:widgetsku,),
       ),
     );
   }
