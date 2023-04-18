8285c98b16b1135a2c076b59d7460a880f6ea1a5
andi <echo.matuladaandi@gmail.com>
2023-04-18 14:04:24 +0800

    start animatiod container & gesture Detector

--- lib/main.dart
+++ lib/main.dart
@@ -10,45 +10,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgetsku = []; // membuat list
-  int counter = 1;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan ListView"),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.spaceAround, // agar tdk mepet tombol hps&+
-              children: <Widget>[
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        widgetsku.add(Text("Data ke- " + counter.toString(),style: TextStyle(fontSize: 20),));
-                        counter++;
-                      });
-                    },
-                    child: Text('Tambah Data')),
-                ElevatedButton(onPressed: () {
-                  setState(() {
-                    widgetsku.removeLast();
-                    counter--;
-                  });
-                }, child: Text('Hapus Data'))
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgetsku,
-            )
-          ],
-        ),
-      ),
+    return Container(
     );
   }
 }
