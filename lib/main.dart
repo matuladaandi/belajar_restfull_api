ce777fbfc82afa06bd82171a61e912626ab69136
andi <echo.matuladaandi@gmail.com>
2023-04-19 16:02:12 +0800

    start flexible widget project

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,10 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
-
+class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Learn Container Animation & GesturDetector'),
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-                duration: Duration(seconds: 1),
-                color: Color.fromARGB(255, random.nextInt(256),
-                    random.nextInt(156), random.nextInt(156)),
-                width: 50.0 + random.nextInt(101),
-                height: 50.0 + random.nextInt(101)),
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
