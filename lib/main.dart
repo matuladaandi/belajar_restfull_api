8b580a28f51093de6dffba90c176f2e8ce09afcc
andi <echo.matuladaandi@gmail.com>
2023-04-19 17:01:16 +0800

    finish flexible widget project

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,62 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flexible Layout',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flexible Layout'),
+        ),
+        body: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.yellow,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.orange,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Color.fromARGB(255, 7, 4, 0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
